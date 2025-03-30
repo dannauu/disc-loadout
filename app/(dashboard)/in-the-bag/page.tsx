f7b87969c4a17d0a2055be9209d225a20d0de236
Jacob <jacobmbaldwin@gmail.com>
2025-03-30 17:44:17 -0500

    jerm

--- app/(dashboard)/in-the-bag/page.tsx
+++ app/(dashboard)/in-the-bag/page.tsx
@@ -1,9 +1,14 @@
 import { Typography } from '@mui/material'
 import React from 'react'
+import Image from 'next/image'
 
 const InTheBag = () => {
   return (
+    <>
     <Typography>InTheBag</Typography>
+
+    <Image alt='Jeremy Koling - MVP Disc Sports' src="/images/pro-pics/jeremy-koling.jpg" width={1200} height={630} />
+    </>
   )
 }
 
