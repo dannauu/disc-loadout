2df1438aae35fad95923333f1b852da909c17cf2
Daniel Nau <danielnau@Daniels-Mac-mini.local>
2025-03-30 16:35:09 -0500

    added form functionality for the bag builder component

--- app/(dashboard)/bag-builder/page.tsx
+++ app/(dashboard)/bag-builder/page.tsx
@@ -4,36 +4,58 @@ import styles from './page.module.css';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import discBrands from './discBrands';
+import discMolds from './discMolds';
 
 
 const BagBuilder = () => {
+    const [selectedManufacturer, setSelectedManufacturer] = React.useState<string | null>('');
+    React.useEffect(() => {
+        console.log(selectedManufacturer);
+    }, [selectedManufacturer]);
+
     return (
         <div className="main">
             <p>
-                Welcome to the Bag Builder page! Here you can customize your bag by adding items, removing items, and viewing your current selections.   Use the options available to create the perfect bag for your needs.
+                User uploads all discs they own or willing to throw and then they can create a bag with those discs they uploaded or chose. Ask questions, left or right handed, forehand or backend, skill level?
             </p>
             <div className={styles.gridWrapper}>
                 <div className={styles.gridItem}>
-                    <label htmlFor="">Choose a disc manufacturer</label>
-                    <Autocomplete
-                        disablePortal
-                        options={discBrands}
-                        sx={{ width: 300 }}
-                        renderInput={(params) => <TextField  {...params} label="Disc Manufacturer" variant="standard" />}
-                    />
-                    <Autocomplete
-                        disablePortal
-                        options={discBrands}
-                        sx={{ width: 300 }}
-                        renderInput={(params) => <TextField  {...params} label="Disc Mold" variant="standard" />}
-                    />
+                    <div>
+                        <label htmlFor="">How long have you been playing disc golf?</label>
+                        <Autocomplete
+                            disablePortal
+                            options={['Less than a year', '1-3 years', '3-5 years', '5-10 years', '10+ years']}
+                            sx={{ width: 300 }}
+                            renderInput={(params) => <TextField  {...params} label="Skill level" variant="standard" />}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="">Choose a disc manufacturer:</label>
+                        <Autocomplete
+                            disablePortal
+                            options={discBrands}
+                            value={selectedManufacturer}
+                            onChange={(event: any, newValue: string | null) => {
+                                setSelectedManufacturer(newValue);
+                            }}
+                            sx={{ width: 300 }}
+                            renderInput={(params) => (
+                                <TextField {...params} label="Disc Manufacturer" variant="standard" />
+                            )}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="">Choose a disc mold:</label>
+                        {/* <Autocomplete
+                            disablePortal
+                            options={discMolds}
+                            sx={{ width: 300 }}
+                            renderInput={(params) => (
+                                <TextField {...params} label="Disc Mold" variant="standard" />
+                            )}
+                        /> */}
+                    </div>
                 </div>
-                <div className={styles.gridItem}></div>
-                <div className={styles.gridItem}></div>
-                <div className={styles.gridItem}></div>
-                <div className={styles.gridItem}></div>
-                <div className={styles.gridItem}></div>
-                <div className={styles.gridItem}></div>
                 <div className={styles.gridItem}></div>
                 <div className={styles.gridItem}></div>
                 <div className={styles.gridItem}></div>
